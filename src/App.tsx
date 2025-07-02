328c281d28681262dbdbe8b5d67c02d004a02db2
Piyush Singh <b221037@iiit-bh.ac.in>
2025-07-03 02:48:13 +0530

    Header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import Header from "./components/layout/Header";
 import Layout from "./components/layout/Layout"
 
 const App =() => {
   return(
     <Layout>
-      <h1>Hello From Layout!!</h1>
-      <p>This is Piyush Singh.</p>
+      <Header title="PiyushSingh" subtitle="Hello from my side"/>
+      <main>
+        <p>Welcome to my portfolio website!</p>
+        <p>Here you can find information about my projects, skills, and experience.</p>
+        <p>Feel free to explore and get in touch!</p>
+      </main>
     </Layout>
   );
 };
